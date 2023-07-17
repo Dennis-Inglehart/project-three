4c908d6a78b2db43f06310b986ead666570bf2b1
Joe Kraemer <joekraemer149@gmail.com>
2023-07-17 16:32:33 -0400

    added axios to make request

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,14 @@
 var https = require('follow-redirects').https;
 var fs = require('fs');
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: "https://developer.nps.gov" 
+});
 
 require('dotenv').config();
 
-window.apiResponse = null; // shit from chatGPT
+window.apiResponse = null; // shit from chatGPT (don't use that) 
 var apiKey = process.env.npsApiKey;
 var options = {
   'method': 'GET',
@@ -15,22 +20,33 @@ var options = {
 };
 console.log(options);
 
-var req = https.request(options, function (res) {
-  var chunks = [];
-
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-
-  res.on("end", function (chunk) {
-    var body = Buffer.concat(chunks);
-    console.log(body.toString());
-    globalThis.apiResponse = JSON.parse(body.toString());
-  });
-
-  res.on("error", function (error) {
-    console.error(error);
-  });
+const makeRequest = () => {
+//   client.get('?_limit=10').then((response) => {
+//     setPosts(response.data);
+//  });
+client.get(`/api/v1/parks?api_key=${apiKey}`).then((response) => {
+return response.data
 });
+}
 
-req.end();
+// var req = https.request(options, function (res) {
+//   var chunks = [];
+
+//   res.on("data", function (chunk) {
+//     chunks.push(chunk);
+//   });
+
+//   res.on("end", function (chunk) {
+//     var body = Buffer.concat(chunks);
+//     console.log(body.toString());
+//     globalThis.apiResponse = JSON.parse(body.toString());
+//   });
+
+//   res.on("error", function (error) {
+//     console.error(error);
+//   });
+// });
+
+// req.end();
+
+module.exports = makeRequest
