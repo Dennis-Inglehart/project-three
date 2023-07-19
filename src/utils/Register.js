eef38f1d983d246ccb9c5804a00623c8e36e8e18
iam3dski <123909952+iam3dski@users.noreply.github.com>
2023-07-19 19:12:19 -0400

    updated the css and register.js file to feel similar to the main page

--- src/utils/Register.js
+++ src/utils/Register.js
@@ -1,15 +1,22 @@
 import {Link} from "react-router-dom";
 
-export default function Register()
-{
-    return(
-        <form className="register">
+export default function Register() {
+    return (
+      <main className="register-page">
+        <header className="app-header">
+          <div>
             <Link to="/">Back</Link>
-            <h1>Signup</h1>
-        <input type="text" placeholder="Enter Username"></input>
-        <input type="password" placeholder="Enter Password"></input>
-        <button>Register</button>
-    
-        </form>
+          </div>
+          <div>Signup</div>
+          <div></div> {/* This empty div is used to center "Signup" text */}
+        </header>
+        <div className="register-container">
+          <form className="register-form">
+            <input type="text" placeholder="Enter Username" />
+            <input type="password" placeholder="Enter Password" />
+            <button>Register</button>
+          </form>
+        </div>
+      </main>
     );
-}
+  }
