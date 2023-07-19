c4c399ae8d4c2a39ec17f0df46e51e0f31c927bd
Mostafa Merhi <mostafamerhi@Mostafas-MacBook-Pro.local>
2023-07-18 20:13:02 -0400

    css and some added to app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
+// import ChooseParks from "./utils/ChooseParks";
+
+// function App()
+// {
+//   return(
+//     <main>
+//       <header>
+//         <div>National Parks App</div>
+//         <nav>
+//         <a href="Login">Login</a>
+//         <a href="Profile">Profile</a>
+//         </nav>
+
+//       </header>
+//       <ChooseParks></ChooseParks>
+//     </main>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+//import React, { useEffect } from 'react';
+//import CarComponent from './components/CarComponent';
+// Importing our theme provider which will make our global state available to child components
+//import CarProvider from './utils/CarContext';
+
+//export default function App() {
+ // useEffect(() => {
+ //   document.title = 'Travel App';
+  //}, []);
+
+ // return (
+    //<CarProvider>
+   //   <CarComponent />
+   // </CarProvider>
+ // );
+//}
 import ChooseParks from "./utils/ChooseParks";
+import "./app.css";
+
+
+
 
 function App()
 {
   return(
     <main>
       <header>
-        <div>National Parks App</div>
+        <div id='header'>National Parks App</div>
         <nav>
-        <a href="Login">Login</a>
-        <a href="Profile">Profile</a>
+        <a href="">Login</a>
+        <a href="">signup</a>
         </nav>
-
       </header>
       <ChooseParks></ChooseParks>
     </main>
