95049b83725452bbb0a026bea047446d5a4a07ff
Andrew Gomoll <andrewgomoll1@gmail.com>
2023-07-19 22:21:19 -0400

    pushing signup route broken

--- src/utils/Register.js
+++ src/utils/Register.js
@@ -1,13 +1,31 @@
 import {Link} from "react-router-dom";
+import {useState} from 'react';
+
 
 export default function Register()
 {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    try{
+    async function registerFunction(ev){
+    ev.preventDefault();
+    await fetch('http://localhost:4000/register', {
+        method: 'POST',
+        body: JSON.stringify({userName, password}),
+        headers: {'Content-Type':'application/json'},
+    })
+    }
+}
+catch(err)
+{
+    console.log(err);
+};
     return(
-        <form className="register">
+        <form className="register" onSubmit={registerFunction}>
             <Link to="/">Back</Link>
             <h1>Signup</h1>
-        <input type="text" placeholder="Enter Username"></input>
-        <input type="password" placeholder="Enter Password"></input>
+        <input type="text" placeholder="Enter Username" value={userName} onChange={ev => setUserName(ev.target.value)}></input>
+        <input type="password" placeholder="Enter Password" value={password} onChange={ev =>setPassword(ev.target.value)}></input>
         <button>Register</button>
     
         </form>
